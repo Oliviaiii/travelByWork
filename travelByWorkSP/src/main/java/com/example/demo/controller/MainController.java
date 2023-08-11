3e6c363103aa67734e97259fd0072f285db2a3d0
oliviaiii <rainy57091224@gmail.com>
2023-08-11 10:35:34 +0800

    SB version 0811
    Springboot版本 更新於20230811

---
+++ travelByWorkSP/src/main/java/com/example/demo/controller/MainController.java
@@ -0,0 +1,81 @@
+package com.example.demo.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+public class MainController {
+   
+	@RequestMapping("homePage")
+	public void homePage(HttpServletResponse response) throws IOException
+	{
+		response.sendRedirect("homePage.html");
+		
+	}
+	@RequestMapping("what")
+    public void what(HttpServletResponse response) throws IOException {
+        response.sendRedirect("what.html");
+    }
+
+    @RequestMapping("helperLogin")
+    public void helpLogin(HttpServletResponse response) throws IOException {
+        response.sendRedirect("helperLogin.html");
+    }
+
+    @RequestMapping("storeLogin")
+    public void storeLogin(HttpServletResponse response) throws IOException {
+        response.sendRedirect("storeLogin.html");
+    }
+
+    @RequestMapping("postJob")
+    public void postJob(HttpServletResponse response) throws IOException {
+        response.sendRedirect("postJob.html");
+    }
+
+    @RequestMapping("share")
+    public void share(HttpServletResponse response) throws IOException {
+        response.sendRedirect("share.html");
+    }
+
+    @RequestMapping("helperSignUp")
+    public void helperSignUp(HttpServletResponse response) throws IOException {
+        response.sendRedirect("helperSignUp.html");
+    }
+
+    @RequestMapping("storeSignUp")
+    public void storeSignUp(HttpServletResponse response) throws IOException {
+        response.sendRedirect("storeSignUp.html");
+    }
+
+    @RequestMapping("contactUs")
+    public void contactUs(HttpServletResponse response) throws IOException {
+        response.sendRedirect("contactUs.html");
+    }
+
+    @RequestMapping("emailSuccess")
+    public void emailSuccess(HttpServletResponse response) throws IOException {
+        response.sendRedirect("emailSuccess.html");
+    }
+
+    @RequestMapping("forgetPassword")
+    public void forgetPassword(HttpServletResponse response) throws IOException {
+        response.sendRedirect("forgetPassword.html");
+    }
+
+    @RequestMapping(value = "helperAccount", method = RequestMethod.POST)
+    public void helperAccount(HttpServletResponse response) throws IOException {
+        response.sendRedirect("helperAccount.html"); 
+    }
+
+
+}
